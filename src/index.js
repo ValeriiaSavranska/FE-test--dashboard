654c762ecd04162ea6f7eac6d63320d362db0125
Valeriia Savranska <lerarog1997@gmail.com>
2024-04-12 14:37:35 +0300

    fix info.json

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const per_page = 8;
 const pageNumbers = [];
 
 const getData = async () => {
-  const response = await fetch('../src/data/info.json');
+  const response = await fetch('../info.json');
   const data = await response.json();
 
   return data;
